9210289ff459a58a2e77102feca325adc91f512c
QDSG <51124389+QDSG@users.noreply.github.com>
2021-02-07 19:58:31 +0800

    Update ceshi.js
    公司限制> <只能分段上传

--- ceshi.js
+++ ceshi.js
@@ -1,1 +1,26 @@
 
+    handleChick() {
+
+    }
+    showPlayPro() {
+        const { number, limits: { max ,min } ,flag, preN } = this.state;
+        const { pre, next } = preN;
+        let str = '输入数字的范围有误，请输入' + min +' ~ ' + max + '的数字(不含' + max + ')';
+        let obj = preN;
+        if( number > min && number < max) {
+            if(number < flag) {
+                obj.pre = number;
+                str = '请输入' + number +' ~ ' + next + '的数字'
+            } else if (number > flag){
+                obj.next = number;
+                str = '请输入' + pre +' ~ ' + number + '的数字'
+            } else {
+                str = '恭喜你答对了，数字是' + flag;
+                this.pageChange('playE');//切换到惩罚环节
+            }
+        }
+        this.setState({
+            preN: obj,
+            message : str
+        })
+    }
