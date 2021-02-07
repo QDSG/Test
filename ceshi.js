5133249fbd2fb1aaf09c3bc10d8e80b33ee17e9b
QDSG <51124389+QDSG@users.noreply.github.com>
2021-02-07 19:59:23 +0800

    Update ceshi.js
    公司限制> <只能分段上传

--- ceshi.js
+++ ceshi.js
@@ -49,3 +49,43 @@
             console.log(this.state);
         });
     }
+
+    render(){
+        const { changePage ,limits, number, flag, message } = this.state;
+        return (
+            <div>
+                { changePage.setM && (
+                    <div>
+                        <label>数字范围：</label>
+                        <input value={ limits.min || '' } onChange={ (e) => this.handleChange(e, 'limits', 'min') }></input> ~
+                        <input value={ limits.max || '' } onChange={ (e) => this.handleChange(e, 'limits', 'max') }></input>
+                        <button onClick={  () => this.setLimits( limits.min, limits.max ) }>确定</button>
+                        <button onClick={  () => this.setLimits( 0, 100 ) }>0~100</button>
+                        <button onClick={  () => this.setLimits( 0, 200 ) }>0~200</button>
+                    </div> 
+                )}
+                { changePage.setFlag && (
+                    <div>
+                        <input value={ flag || '' } onChange={ (e) => this.handleChange(e, 'flag') }></input>
+                        <button onClick={ this.setFlagNum }>确定</button>
+                        <button onClick={ this.randomInt}>随机数</button>
+                        <p>{ message }</p>
+                    </div>
+                )}
+                { changePage.playS && (
+                    <div>
+                        <input value={ number || '' } onChange={ (e) => this.handleChange(e, 'number') }></input>
+                        <button onClick={ this.showPlayPro }>确定</button>
+                        <p>{ message }</p>
+                    </div>
+                )}
+                { changePage.playE && (
+                    <div>
+                        <p>{ message }</p>
+                    </div>
+                )}
+            </div>
+        )
+    }
+}
+export default Test;
