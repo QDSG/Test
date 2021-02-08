efd98146bc19d834758721e97177d34439338bc8
QDSG <51124389+QDSG@users.noreply.github.com>
2021-02-08 10:04:33 +0800

    Update ceshi.js
    添加ands的组件

--- ceshi.js
+++ ceshi.js
@@ -1,30 +1,4 @@
 
-    handleChick() {
-
-    }
-    showPlayPro() {
-        const { number, limits: { max ,min } ,flag, preN } = this.state;
-        const { pre, next } = preN;
-        let str = '输入数字的范围有误，请输入' + min +' ~ ' + max + '的数字(不含' + max + ')';
-        let obj = preN;
-        if( number > min && number < max) {
-            if(number < flag) {
-                obj.pre = number;
-                str = '请输入' + number +' ~ ' + next + '的数字'
-            } else if (number > flag){
-                obj.next = number;
-                str = '请输入' + pre +' ~ ' + number + '的数字'
-            } else {
-                str = '恭喜你答对了，数字是' + flag;
-                this.pageChange('playE');//切换到惩罚环节
-            }
-        }
-        this.setState({
-            preN: obj,
-            message : str
-        })
-    }
-
     randomInt() {
         console.log(this.state.limits)
         const { max ,min } = this.state.limits;
@@ -57,25 +31,35 @@
                 { changePage.setM && (
                     <div>
                         <label>数字范围：</label>
-                        <input value={ limits.min || '' } onChange={ (e) => this.handleChange(e, 'limits', 'min') }></input> ~
+                        <InputNumber value={ limits.min || '' } onChange={ (e) => this.handleChange(e, 'limits', 'min') }></InputNumber> ~
+                        <InputNumber value={ limits.max || '' } onChange={ (e) => this.handleChange(e, 'limits', 'max') }></InputNumber>
+                        <Button onClick={  () => this.setLimits( limits.min, limits.max ) }>确定</Button>
+                        <Button onClick={  () => this.setLimits( 0, 100 ) }>0~100</Button>
+                        <Button onClick={  () => this.setLimits( 0, 200 ) }>0~200</Button>
+                        {/* <input value={ limits.min || '' } onChange={ (e) => this.handleChange(e, 'limits', 'min') }></input> ~
                         <input value={ limits.max || '' } onChange={ (e) => this.handleChange(e, 'limits', 'max') }></input>
                         <button onClick={  () => this.setLimits( limits.min, limits.max ) }>确定</button>
                         <button onClick={  () => this.setLimits( 0, 100 ) }>0~100</button>
-                        <button onClick={  () => this.setLimits( 0, 200 ) }>0~200</button>
+                        <button onClick={  () => this.setLimits( 0, 200 ) }>0~200</button> */}
                     </div> 
                 )}
                 { changePage.setFlag && (
                     <div>
-                        <input value={ flag || '' } onChange={ (e) => this.handleChange(e, 'flag') }></input>
+                        <InputNumber value={ flag || '' } onChange={ (e) => this.handleChange(e, 'flag') }></InputNumber>
+                        <Button onClick={  () => this.setFlagNum }>确定</Button>
+                        <Button onClick={  () => this.randomInt}>随机数</Button>
+                        {/* <input value={ flag || '' } onChange={ (e) => this.handleChange(e, 'flag') }></input>
                         <button onClick={ this.setFlagNum }>确定</button>
-                        <button onClick={ this.randomInt}>随机数</button>
+                        <button onClick={ this.randomInt}>随机数</button> */}
                         <p>{ message }</p>
                     </div>
                 )}
                 { changePage.playS && (
                     <div>
-                        <input value={ number || '' } onChange={ (e) => this.handleChange(e, 'number') }></input>
-                        <button onClick={ this.showPlayPro }>确定</button>
+                        <InputNumber value={ number || '' } onChange={ (e) => this.handleChange(e, 'number') }></InputNumber>
+                        <Button onClick={  () => this.showPlayPro }>确定</Button>
+                        {/* <input value={ number || '' } onChange={ (e) => this.handleChange(e, 'number') }></input>
+                        <button onClick={ this.showPlayPro }>确定</button> */}
                         <p>{ message }</p>
                     </div>
                 )}
