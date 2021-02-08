aab40036c8bfe8acb6096797756c91f7b6a7cf23
QDSG <51124389+QDSG@users.noreply.github.com>
2021-02-08 10:03:39 +0800

    Update digitalBomb.js
    合理分配分段代码空间

--- digitalBomb.js
+++ digitalBomb.js
@@ -26,11 +26,6 @@ class Test extends React.Component {
             },
             message: '',
         }
-        this.handleChange = this.handleChange.bind(this);
-        this.setLimits = this.setLimits.bind(this);
-        this.randomInt = this.randomInt.bind(this);
-        this.setFlagNum = this.setFlagNum.bind(this);
-        this.showPlayPro = this.showPlayPro.bind(this);
     }
 
     componentDidMount() {
@@ -80,4 +75,26 @@ class Test extends React.Component {
                 console.log(this.state);
             });
         }
+    }
+    showPlayPro() {
+        const { number, limits: { max ,min } ,flag, preN } = this.state;
+        const { pre, next } = preN;
+        let str = '输入数字的范围有误，请输入' + min +' ~ ' + max + '的数字(不含' + max + ')';
+        let obj = preN;
+        if( number > min && number < max) {
+            if(number < flag) {
+                obj.pre = number;
+                str = '请输入' + number +' ~ ' + next + '的数字'
+            } else if (number > flag){
+                obj.next = number;
+                str = '请输入' + pre +' ~ ' + number + '的数字'
+            } else {
+                str = '恭喜你答对了，数字是' + flag;
+                this.pageChange('playE');//切换到惩罚环节
+            }
+        }
+        this.setState({
+            preN: obj,
+            message : str
+        })
     }
