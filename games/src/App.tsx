57c8f2ceef80ce352357367e9c7c9fe6b933c69b
gc <454572457@qq.com>
2023-11-14 01:07:15 +0800

    feat: 提交一个react做的游戏（现在还只有数字炸弹）

---
+++ games/src/App.tsx
@@ -0,0 +1,14 @@
+import DigitalBomb from './components/DigitalBomb';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <DigitalBomb />
+      </header>
+    </div>
+  );
+}
+
+export default App;
